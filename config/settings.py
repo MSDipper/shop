afbae242d0d7727859caf65bafee59cc8121f232
MSDipper <github5248@gmail.com>
2022-10-10 22:32:36 +0300

    add settings ckeditor

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'django-ckeditor',
+    'ckeditor',
     
     'blog.apps.BlogConfig',
 ]
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
