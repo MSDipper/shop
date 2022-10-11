e06f4c8c5d76f36fd94872dd6f3c1cff221a10d7
MSDipper <github5248@gmail.com>
2022-10-11 18:02:56 +0300

    added template post_list.html and add view template settings and style settings, add models in admin.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def blog(request):
+    post_list = Post.objects.all()
+    context = {
+        'post_list':post_list,
+    }
+    return render(request, 'blog/post_list.html', context)
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
