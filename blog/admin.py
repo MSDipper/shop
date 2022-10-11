7b42e69b3f91bc2738aa11ed212ddb9cddc5a939
MSDipper <github5248@gmail.com>
2022-10-11 19:43:29 +0300

    update admon.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 
 from blog.models import Category, Tag, Ip, Post, Comment
 
-admin.site.register(Category)
-admin.site.register(Tag)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+    save_as = True
+    save_on_top = True
+    
+   
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+    save_as = True
+    save_on_top = True 
+    
+   
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',), }
+    save_as = True
+    save_on_top = True 
+
 admin.site.register(Ip)
-admin.site.register(Post)
 admin.site.register(Comment)
