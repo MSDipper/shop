003307456568cb6806ca58645fa3092e76bf1673
MSDipper <github5248@gmail.com>
2022-10-17 18:32:37 +0300

    added posts in template

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from shop.models import Product
 
 def shop(request):
-    product = Product.objects.all()
+    product = Product.objects.all()[:6]
     context = {
         'product_list':product,
     }
