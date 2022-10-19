7dd6f4ef55cb4802560b9cffeeebcc106e636910
MSDipper <github5248@gmail.com>
2022-10-19 19:55:08 +0300

    added choice integer in forms in link cart and create app orders for sort product users

--- cart/forms.py
+++ cart/forms.py
@@ -1,10 +1,14 @@
 from django import forms
+from shop.models import Product
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
+    
     quantity = forms.IntegerField(min_value=1)
     
-    # quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    # # quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+    
