25f092d71d6fdcec4a3ef9f66d7f487771ebc90e
MSDipper <github5248@gmail.com>
2022-10-21 19:27:37 +0300

    create models for orders

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    "phonenumber_field",
     'ckeditor',
     
+    'orders.apps.OrdersConfig',
     'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
     'blog.apps.BlogConfig',
@@ -69,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+class OrderItemList(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    company_name = models.CharField(max_length=150, verbose_name='Компания')
+    phone = PhoneNumberField(verbose_name='Телефон')
+    address = models.CharField(verbose_name='Адрес', max_length=150)
+    city = models.CharField(verbose_name='Город', max_length=150)
+
+    class Meta:
+        verbose_name = 'Данные заказчика'
+        verbose_name_plural = 'Данные заказчика'
+        
+    def __str__(self):
+        return self.first_name
+    
