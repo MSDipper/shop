5bff97d57f5ef6f021bd3fcf55d96d156c386f65
MSDipper <github5248@gmail.com>
2022-10-29 21:33:02 +0300

    create new model for coupons app and migrate model

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,22 @@
+from tabnanny import verbose
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    ''' Купон пользователя '''
+    code = models.CharField(max_length=50, unique=True, verbose_name='Код с купона')
+    valid_from = models.DateTimeField(verbose_name='Время действительности купона')
+    valid_to = models.DateTimeField(verbose_name='Время недействительности купона')
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name='Процент скидки купона')
+    active = models.BooleanField(verbose_name='Активность купона')
+    
+    
+    class Meta:
+        verbose_name = 'Купон'
+        verbose_name_plural = 'Купоны'
+        
+    
+    def __str__(self):
+        return self.code
+    
