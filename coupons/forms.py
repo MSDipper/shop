aea6644475c267fa6e0bfc78ed18c6e4126ec2b1
MSDipper <github5248@gmail.com>
2022-10-30 19:20:38 +0300

    update code in templates

--- coupons/forms.py
+++ coupons/forms.py
@@ -3,6 +3,9 @@ from django import forms
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField(
-        label='Код с купона',
-        widget=forms.TextInput(attrs={'class':"gray_btn"})
-        )
+                        label='',
+                        widget=forms.TextInput(attrs={
+                                        'class':"gray_btn",
+                                        'placeholder':"Coupon Code"
+                                        })
+                        )
