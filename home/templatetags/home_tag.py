0eaf6f78429cc374860fdb217359c0b385c07bf7
MSDipper <github5248@gmail.com>
2022-11-01 20:48:57 +0300

    added view product in home

---
+++ home/templatetags/home_tag.py
@@ -0,0 +1,11 @@
+from django import template
+from shop.models import Product
+from django.db.models import Count
+
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_products():
+    return Product.objects.all()
