285c4a8c3fa47ab65c8f750b1fcc05117148e44c
MSDipper <github5248@gmail.com>
2022-11-01 19:38:25 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from blog.models import Post
 from shop.models import Product
 
