9aaeb23e1696a9f7d73b6b2f2ab5691929d02a85
MSDipper <github5248@gmail.com>
2022-11-02 16:55:45 +0300

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -1,20 +1,29 @@
 from django.db import models
 from django.urls import reverse
 from ckeditor.fields import RichTextField
+from mptt.models import MPTTModel, TreeForeignKey
 
-
-class Category(models.Model):
+class Category(MPTTModel):
     """ Категория """
     name = models.CharField(max_length=150, verbose_name='Имя')
     slug = models.SlugField(max_length=150, verbose_name='URL', unique=True)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+        )
     
     
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         
+        
     def get_absolute_url(self):
         return reverse('product_list_by_category', args=[self.slug])
+    
     
     def __str__(self):
         return self.name        
