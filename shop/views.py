2e8c39d481e90c773eb86073c3ef14efadd77170
MSDipper <github5248@gmail.com>
2022-11-04 18:56:51 +0300

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -21,7 +21,7 @@ class ShopListView(BrandColor, ListView):
     template_name = 'shop/product_list.html'
     
     def get_queryset(self):
-        return Product.objects.filter(published=True, category__slug=self.kwargs.get("slug")).select_related('category')
+        return Product.objects.filter(published=True).select_related('category')
     
     
     
