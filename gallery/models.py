b3fa291d77ccb4b19a7fb3cf931ba6b60f8d6787
MSDipper <github5248@gmail.com>
2022-11-07 18:44:05 +0300

    add models in gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class MyLife(models.Model):
+    ''' О жизни '''
+    title = models.CharField(verbose_name='Заголовок', max_length=50)
+    text = models.TextField(verbose_name='Текст')
+    
+    
+    class Meta:
+        verbose_name = 'Моя жизнь'
+        verbose_name_plural = 'Моя жизнь'
+        
+        
+    def __str__(self):
+        return f'{self.title}'
+
+
+class ImageStyleLife(models.Model):
+    ''' Моя Галерея '''
+    image = models.ImageField(
+        upload_to='image_style_life/',
+        blank=True,
+        verbose_name='Фото'
+        )
+    
+    
+    class Meta:
+        verbose_name = 'Мой стиль'
+        verbose_name_plural = 'Мой стиль'
+        
+        
+    def __str__(self):
+        return f'{self.image}'
