51f29473b93868d97853f5f8d5dedb9673b5b61c
MSDipper <github5248@gmail.com>
2022-11-09 18:40:53 +0300

    added model for our contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Contact(models.Model):
+    address = models.CharField(verbose_name='Адрес', max_length=50)
+    phone = PhoneNumberField(verbose_name='Телефон')
+    email = models.EmailField(verbose_name='Email', max_length=254)
+    
+
+    class Meta:
+        verbose_name = 'Наши контакты'
+        verbose_name_plural = 'Наши контакты'
+        
+        
+    def __str__(self):
+        return f'{self.address}'    
