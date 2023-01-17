3bf7ed6e544162a1ee11dc5ed058a3155e23bf2b
MSDipper <github5248@gmail.com>
2023-01-17 19:00:09 +0300

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 from contextlib import redirect_stderr
 from django.urls import path
-from account.views import RegisterUser, PagesPage
+from account.views import RegisterUser, AccountPage
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('login/', LoginView.as_view(template_name='account/register/login.html'), name='login'),
     path('register_user/', RegisterUser.as_view(), name='register'),
-    path('', PagesPage.as_view(), name='pages'),
+    path('', AccountPage.as_view(), name='pages'),
 ]
